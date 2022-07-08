d33577c855eff16139be5d0c05310c227b99fe66
Giuseppe Cicalini <cicone@gmail.com>
2022-07-08 06:32:55 +0000

    New: updated to py3 only.

--- oauth2app/models.py
+++ oauth2app/models.py
@@ -1,27 +1,31 @@
-# -*- coding: utf-8 -*-
-
-
 """OAuth 2.0 Django Models"""
 import sys
 import time
 from hashlib import sha512
 from uuid import uuid4
-from django.db import models
+
 from django.conf import settings
+from django.db import models
 from jsonfield.fields import JSONField
 
-from .consts import CLIENT_KEY_LENGTH, CLIENT_SECRET_LENGTH
-from .consts import SCOPE_LENGTH
-from .consts import ACCESS_TOKEN_LENGTH, REFRESH_TOKEN_LENGTH
-from .consts import ACCESS_TOKEN_EXPIRATION, MAC_KEY_LENGTH, REFRESHABLE
-from .consts import CODE_KEY_LENGTH, CODE_EXPIRATION
-
+from .consts import (
+    ACCESS_TOKEN_EXPIRATION,
+    ACCESS_TOKEN_LENGTH,
+    CLIENT_KEY_LENGTH,
+    CLIENT_SECRET_LENGTH,
+    CODE_EXPIRATION,
+    CODE_KEY_LENGTH,
+    MAC_KEY_LENGTH,
+    REFRESHABLE,
+    REFRESH_TOKEN_LENGTH,
+    SCOPE_LENGTH,
+)
 
 PY3 = sys.version_info[0] == 3
-AUTH_USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
+AUTH_USER_MODEL = getattr(settings, "AUTH_USER_MODEL", "auth.User")
 
 
-class TimestampGenerator(object):
+class TimestampGenerator:
     """Callable Timestamp Generator that returns a UNIX time integer.
 
     **Kwargs:**
@@ -31,6 +35,7 @@ class TimestampGenerator(object):
 
     *Returns int*
     """
+
     def __init__(self, seconds=0):
         self.seconds = seconds
 
@@ -38,10 +43,10 @@ class TimestampGenerator(object):
         return int(time.time()) + self.seconds
 
     def deconstruct(self):
-        return ('oauth2app.oauth2app.models.TimestampGenerator', [], {})
+        return ("oauth2app.oauth2app.models.TimestampGenerator", [], {})
 
 
-class KeyGenerator(object):
+class KeyGenerator:
     """Callable Key Generator that returns a random keystring.
 
     **Args:**
@@ -50,23 +55,19 @@ class KeyGenerator(object):
 
     *Returns str*
     """
+
     def __init__(self, length=16):
         self.length = length
 
     def __call__(self):
-        return sha512(uuid4().hex.encode('utf-8')).hexdigest()[0:self.length]
+        return sha512(uuid4().hex.encode("utf-8")).hexdigest()[0 : self.length]
 
     def deconstruct(self):
-        kwargs = {'length': 30}
-        return ('oauth2app.oauth2app.models.KeyGenerator', [], kwargs)
+        kwargs = {"length": 30}
+        return ("oauth2app.oauth2app.models.KeyGenerator", [], kwargs)
 
 
-class UnicodeModelMixin(object):
-    def __str__(self):
-        return self.__unicode__() if PY3 else super(UnicodeModelMixin, self).__str__()
-
-
-class Client(UnicodeModelMixin, models.Model):
+class Client(models.Model):
     """Stores client authentication data.
 
     **Args:**
@@ -87,18 +88,12 @@ class Client(UnicodeModelMixin, models.Model):
       *Default None*
 
     """
+
     name = models.CharField(max_length=256)
     user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
     description = models.TextField(null=True, blank=True)
-    key = models.CharField(
-        unique=True,
-        max_length=CLIENT_KEY_LENGTH,
-        default=KeyGenerator(CLIENT_KEY_LENGTH),
-        db_index=True)
-    secret = models.CharField(
-        unique=True,
-        max_length=CLIENT_SECRET_LENGTH,
-        default=KeyGenerator(CLIENT_SECRET_LENGTH))
+    key = models.CharField(unique=True, max_length=CLIENT_KEY_LENGTH, default=KeyGenerator(CLIENT_KEY_LENGTH), db_index=True)
+    secret = models.CharField(unique=True, max_length=CLIENT_SECRET_LENGTH, default=KeyGenerator(CLIENT_SECRET_LENGTH))
     redirect_uris = JSONField(null=True)
     approved = models.BooleanField(default=True)
 
@@ -107,11 +102,11 @@ class Client(UnicodeModelMixin, models.Model):
         if self.redirect_uris:
             return self.redirect_uris[0]
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
 
-class AccessRange(UnicodeModelMixin, models.Model):
+class AccessRange(models.Model):
     """Stores access range data, also known as scope.
 
     **Args:**
@@ -125,17 +120,18 @@ class AccessRange(UnicodeModelMixin, models.Model):
       *Default None*
 
     """
+
     key = models.CharField(unique=True, max_length=SCOPE_LENGTH, db_index=True)
     description = models.TextField(blank=True)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.key
 
     class Meta:
         ordering = ('id', )
 
 
-class AccessToken(UnicodeModelMixin, models.Model):
+class AccessToken(models.Model):
     """Stores access token data.
 
     **Args:**
@@ -157,39 +153,24 @@ class AccessToken(UnicodeModelMixin, models.Model):
       refreshable. *Default False*
 
     """
+
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
     user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
-    token = models.CharField(
-        unique=True,
-        max_length=ACCESS_TOKEN_LENGTH,
-        default=KeyGenerator(ACCESS_TOKEN_LENGTH),
-        db_index=True)
+    token = models.CharField(unique=True, max_length=ACCESS_TOKEN_LENGTH, default=KeyGenerator(ACCESS_TOKEN_LENGTH), db_index=True)
     refresh_token = models.CharField(
-        unique=True,
-        blank=True,
-        null=True,
-        max_length=REFRESH_TOKEN_LENGTH,
-        default=KeyGenerator(REFRESH_TOKEN_LENGTH),
-        db_index=True)
-    mac_key = models.CharField(
-        unique=True,
-        blank=True,
-        null=True,
-        max_length=MAC_KEY_LENGTH,
-        default=None)
-    issue = models.PositiveIntegerField(
-        editable=False,
-        default=TimestampGenerator())
-    expire = models.PositiveIntegerField(
-        default=TimestampGenerator(ACCESS_TOKEN_EXPIRATION))
+        unique=True, blank=True, null=True, max_length=REFRESH_TOKEN_LENGTH, default=KeyGenerator(REFRESH_TOKEN_LENGTH), db_index=True
+    )
+    mac_key = models.CharField(unique=True, blank=True, null=True, max_length=MAC_KEY_LENGTH, default=None)
+    issue = models.PositiveIntegerField(editable=False, default=TimestampGenerator())
+    expire = models.PositiveIntegerField(default=TimestampGenerator(ACCESS_TOKEN_EXPIRATION))
     scope = models.ManyToManyField(AccessRange)
     refreshable = models.BooleanField(default=REFRESHABLE)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.token
 
 
-class Code(UnicodeModelMixin, models.Model):
+class Code(models.Model):
     """Stores authorization code data.
 
     **Args:**
@@ -208,22 +189,16 @@ class Code(UnicodeModelMixin, models.Model):
     * *scope:* A list of oauth2app.models.AccessRange objects. *Default None*
 
     """
+
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
     user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
-    key = models.CharField(
-        unique=True,
-        max_length=CODE_KEY_LENGTH,
-        default=KeyGenerator(CODE_KEY_LENGTH),
-        db_index=True)
-    issue = models.PositiveIntegerField(
-        editable=False,
-        default=TimestampGenerator())
-    expire = models.PositiveIntegerField(
-        default=TimestampGenerator(CODE_EXPIRATION))
+    key = models.CharField(unique=True, max_length=CODE_KEY_LENGTH, default=KeyGenerator(CODE_KEY_LENGTH), db_index=True)
+    issue = models.PositiveIntegerField(editable=False, default=TimestampGenerator())
+    expire = models.PositiveIntegerField(default=TimestampGenerator(CODE_EXPIRATION))
     redirect_uri = models.URLField(null=True)
     scope = models.ManyToManyField(AccessRange)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.key
 
 
@@ -236,5 +211,6 @@ class MACNonce(models.Model):
     * *nonce:* A unique nonce string.
 
     """
+
     access_token = models.ForeignKey(AccessToken, on_delete=models.CASCADE)
     nonce = models.CharField(max_length=30, db_index=True)
